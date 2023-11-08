b9f1f33519b88acd9ae70dfa054dff4d4201c5d3
Gboyega Idowu <gboyegaidowu@Gboyegas-MacBook-Pro.local>
2023-11-08 14:21:43 +0000

    New update commite with Item and Menu

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const db = require("../db/connection");
+const Menu = require("./menu");
 
 const Restaurant = db.define("restaurants", {
     name: Sequelize.STRING,
     location: Sequelize.STRING,
     cuisine: Sequelize.STRING
 })
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll({});
+    res.json(restaurants, Menu);
+});
 
 module.exports = Restaurant;
